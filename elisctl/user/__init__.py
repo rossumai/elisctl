f3328979937638fe2a9264b899bc4681c970d357
Kryštof Pilnáček <krystof.pilnacek@rossum.ai>
2019-02-05 09:56:12 +0100

    Add user change command

--- elisctl/user/__init__.py
+++ elisctl/user/__init__.py
@@ -1,6 +1,11 @@
+from typing import Tuple, Optional, Dict, Any
+
 import click
 
+from elisctl.lib.api_client import APIClient, get_json
 from elisctl.user import create
+from elisctl.user.helpers import get_groups
+from elisctl.user.options import queue_option, group_option, locale_option, password_option
 
 
 @click.group("user")
@@ -9,3 +14,37 @@ def cli() -> None:
 
 
 cli.add_command(create.create_command)
+
+
+@cli.command(name="change", help="Change a user.")
+@click.argument("id_", metavar="ID", type=str)
+@queue_option
+@group_option(default=None, show_default=False)
+@locale_option(default=None, show_default=False)
+@password_option(help=None)
+def change_command(
+    id_: str,
+    queue_id: Tuple[str],
+    group: Optional[str],
+    locale: Optional[str],
+    password: Optional[str],
+) -> None:
+    if not any([queue_id, group, locale, password]):
+        return
+
+    data: Dict[str, Any] = {}
+    if password is not None:
+        data["password"] = password
+
+    with APIClient() as api_client:
+        if queue_id:
+            data["queues"] = [
+                get_json(api_client.get(f"queues/{queue}"))["url"] for queue in queue_id
+            ]
+        if group is not None:
+            data["groups"] = get_groups(api_client, group)
+        if locale is not None:
+            ui_settings = get_json(api_client.get(f"users/{id_}"))["ui_settings"]
+            data["ui_settings"] = {**ui_settings, "locale": locale}
+
+        api_client.patch(f"users/{id_}", data)
