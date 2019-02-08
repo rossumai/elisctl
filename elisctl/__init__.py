860787451259c514c823d3c1ba5551f8ac2547fc
Kryštof Pilnáček <krystof.pilnacek@rossum.ai>
2019-02-08 19:16:30 +0100

    Add version option

--- elisctl/__init__.py
+++ elisctl/__init__.py
@@ -1,3 +1,4 @@
+import click
 from click_shell import shell
 
 from elisctl import tools, schema, csv, configure, user
@@ -8,6 +9,7 @@ from elisctl import tools, schema, csv, configure, user
     intro="Welcome to the elisctl interactive mode. Start with `help` and `configure`.",
     context_settings={"help_option_names": ["-h", "--help"]},
 )
+@click.version_option()
 def entry_point() -> None:
     pass
 
