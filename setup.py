5349c5a25f818bd097d9c38580b1f463f5e75467
Kryštof Pilnáček <krystof.pilnacek@rossum.ai>
2019-07-12 15:08:05 +0200

    Bump version: 2.4.0 → 2.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="elisctl",
-    version="2.4.0",
+    version="2.5.0",
     description="Command line interface for controlling ELIS",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
