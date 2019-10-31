2d0ac177086746b0c265c6fb272fc2625bc6125f
Michaela Ockova <michaela.ockova@rossum.ai>
2019-10-31 14:58:06 +0100

    Bump version: 2.7.1 → 2.8.0

--- elisctl/__init__.py
+++ elisctl/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.7.1"
+__version__ = "2.8.0"
 
 CTX_PROFILE = "PROFILE"
 CTX_DEFAULT_PROFILE = "default"
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="elisctl",
-    version="2.7.1",
+    version="2.8.0",
     description="Command line interface for controlling the Rossum platform",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
