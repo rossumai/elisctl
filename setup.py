81d0aa66003ba623329160afb8c35b711a83da0e
Michaela Ockova <ockova.michaela@gmail.com>
2020-09-29 19:47:51 +0200

    Add legacy project information

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="elisctl",
@@ -16,7 +16,7 @@ setup(
         "Tracker": "https://github.com/rossumai/elisctl/issues",
     },
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 7 - Inactive",
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
         "License :: OSI Approved :: MIT License",
@@ -26,25 +26,12 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    packages=find_packages(exclude=("tests*",)),
     install_requires=[
-        "pandas",
-        "click",
-        "click-shell",
-        "xlrd",
-        "requests",
-        "jsondiff",
-        "tabulate",
-        'dataclasses;python_version<"3.7"',
-        "openpyxl>=2.6",
-        "jmespath",
-        "polling2",
-        "more-itertools",
+        "rossum",
     ],
     python_requires=">=3.6",
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest", "pytest-cov", "requests_mock", "pytest-click"],
     zip_safe=False,
     entry_points={"console_scripts": ["elisctl = elisctl.main:entry_point"]},
 )
